c1e6c2d28f5985bae03441c0c13f1053bb11bbe0
George <george@Georges-MacBook-Pro-2.local>
2019-10-16 09:42:34 -0400

    adds committe id col to output

--- etl/pgquery.py
+++ etl/pgquery.py
@@ -9,4 +9,4 @@ conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", 
 
 cur = conn.cursor()
 
-cur.copy_expert(f"COPY (SELECT committee_name, election_state, election_district, col_a_total_individual_contributions, col_a_pac_contributions, col_a_candidate_contributions, col_a_candidate_loans, col_a_total_receipts, col_b_total_receipts, col_a_total_disbursements, col_a_cash_on_hand_close FROM fec_campaign_summaries) TO '{wd}/data/processed/{filename}' WITH (FORMAT CSV, HEADER, DELIMITER ',');", sys.stdout)
+cur.copy_expert(f"COPY (SELECT filer_committee_id_number, committee_name, election_state, election_district, col_a_total_individual_contributions, col_a_pac_contributions, col_a_candidate_contributions, col_a_candidate_loans, col_a_total_receipts, col_b_total_receipts, col_a_total_disbursements, col_a_cash_on_hand_close FROM fec_campaign_summaries) TO '{wd}/data/processed/{filename}' WITH (FORMAT CSV, HEADER, DELIMITER ',');", sys.stdout)
