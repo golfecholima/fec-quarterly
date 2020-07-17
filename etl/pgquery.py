1724069b428b9fc0ab2ebe53a8c4f3a68189cdba
George <george@Georges-MBP-3.home>
2020-07-17 15:45:16 -0400

    Moves main work from .sh to .ipynb, annotates

--- etl/pgquery.py
+++ etl/pgquery.py
@@ -1,12 +1,25 @@
 import psycopg2
+import datetime
 import sys
 import os
 
 wd = os.getcwd()
-filename = 'output1.csv'
+d = datetime.datetime.now()
+db = d.strftime("%b%d%Y").lower()
+filename = 'output-' + d.strftime("%Y-%m-%d-%H%M").lower() + '.csv'
 
-conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="")
+# Connect to postgres database
+
+print(f'''
+Connecting to postgres database {db} ...
+''')
+
+conn = psycopg2.connect(host="localhost",database=db, user="postgres", password="")
 
 cur = conn.cursor()
 
-cur.copy_expert(f"COPY (SELECT filing_id, form_type, filer_committee_id_number, committee_name, election_state, election_district, report_code, coverage_from_date, coverage_through_date, col_a_total_individual_contributions, col_a_pac_contributions, col_a_candidate_contributions, col_a_candidate_loans, col_a_total_receipts, col_b_total_receipts, col_a_total_disbursements, col_a_cash_on_hand_close FROM fec_campaign_summaries) TO '{wd}/data/processed/{filename}' WITH (FORMAT CSV, HEADER, DELIMITER ',');", sys.stdout)
+print(f'''
+Exporting data to /data/processed/{filename} ...
+''')
+
+cur.copy_expert(f"COPY (SELECT filing_id, form_type, filer_committee_id_number, committee_name, report_code, coverage_from_date, coverage_through_date, col_a_total_individual_contributions, col_a_individual_contributions_unitemized, col_a_pac_contributions, col_a_candidate_contributions, col_a_candidate_loans, col_a_total_receipts, col_b_total_receipts, col_a_total_disbursements, col_a_cash_on_hand_close FROM fec_campaign_summaries) TO '{wd}/data/processed/{filename}' WITH (FORMAT CSV, HEADER, DELIMITER ',');", sys.stdout)
